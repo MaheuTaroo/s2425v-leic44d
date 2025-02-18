385c39fb91fa26af2dcfac24ba02c735f4ea1481
Paulo Pereira <paulo.pereira@isel.pt>
2025-02-18 15:56:43 +0000

    Adds the code written during today's lecture

---
+++ demos/EchoServer/src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+package org.example
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.BufferedWriter
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+import java.net.Socket
+
+val logger: Logger = LoggerFactory.getLogger("Main")
+
+const val PORT = 9090
+fun main() {
+    logger.info("Starting echo server on port $PORT")
+    runEchoServer()
+    logger.info("Echo server stopped")
+}
+
+fun runEchoServer() {
+    ServerSocket().use { serverSocket ->
+        serverSocket.bind(InetSocketAddress("0.0.0.0", PORT))
+        var clientCount = 0
+        while (true) {
+            logger.info("Waiting for clients to connect...")
+            val clientSocket = serverSocket.accept()
+            Thread.ofPlatform().start {
+                clientCount += 1 // This is wrong! Read-modify-write is not atomic
+                handleClient(clientSocket, clientCount)
+            }
+        }
+    }
+}
+
+fun handleClient(clientSocket: Socket, clientCount: Int) {
+    logger.info("Client connected: ${clientSocket.inetAddress.hostAddress}")
+    clientSocket.use {
+        it.getInputStream().bufferedReader().use { reader ->
+            it.getOutputStream().bufferedWriter().use { writer ->
+                logger.info("Handling client #$clientCount")
+                writer.writeLine("Hello, client #$clientCount! Please type something and press Enter:")
+                val line = reader.readLine()
+                writer.writeLine("You wrote: $line")
+                writer.writeLine("Bye!\n")
+            }
+        }
+    }
+}
+
+private fun BufferedWriter.writeLine(line: String) {
+    write(line)
+    newLine()
+    flush()
+}
---
+++ outline.md
@@ -0,0 +1,25 @@
+
+## Week 1
+### 17/02/2025 - Course introduction
+* Syllabus, teaching methodology and bibliography.
+  * Evaluation
+  * Resources
+* Course information on Moodle (for authenticated users only)
+  * [General information](https://2425moodle.isel.pt/course/view.php?id=9166)
+  * [LEIC44D course section](https://2425moodle.isel.pt/course/view.php?id=9362)
+  * [Playlist with the course's lectures](https://www.youtube.com/playlist?list=PL8XxoCaL3dBhX9Kqt_BfAE23D4zYqgLdN)
+
+### 18/02/2025 - Threading on the JVM: Introduction _(preview)_
+* Threading on the JVM
+  * Purpose and motivation
+  * Platform threads: thread creation and execution
+* Classifying state: 
+  * private - in the thread's stack
+  * shared - stored globally or accessible through a closure
+* Concurrent access to shared mutable state: consequences
+  * The need for synchronization: introduction
+  * Threading hazards: lost updates
+    * Example: loss of increments
+
+For reference: 
+  * Lecture video (in Portuguese) _(coming soon)_
